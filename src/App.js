a39e11c9cc14bfc7fd80f9a9bc64e29efe6a9ee3
hafiz1379 <hafizrasa1379@gmail.com>
2025-08-06 08:35:37 +0430

    Refactor App component to include routing and context providers for product and cart management

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+
+// Components
+import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
+
+// Context
+import ProductContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
-    <div>
-     App Component
-    </div>
+    <ProductContextProvider>
+      <CartContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Store />} />
+          <Route path="/cart" element={<ShopCart />} />
+          <Route path="*" element={<Navigate to="/products" replace />} />
+        </Routes>
+      </CartContextProvider>
+    </ProductContextProvider>
   );
 }
 
