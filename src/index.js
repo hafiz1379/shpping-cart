4e151976e588c1d6a855ab237fe597f785e9cfa3
hafiz1379 <hafizrasa1379@gmail.com>
2025-08-06 08:35:44 +0430

    Update index.js to use ReactDOM.createRoot for rendering with React 18+ syntax

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+// Create root and render (React 18+ syntax)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
